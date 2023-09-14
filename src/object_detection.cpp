54603daec3a4ae21397d798e979ab0de2367d4d1
Jakub Delicat <delicat.kuba@gmail.com>
2023-09-15 00:53:00 +0200

    implemented best parameters
    Signed-off-by: Jakub Delicat <delicat.kuba@gmail.com>

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -9,11 +9,11 @@ void ObjectDetection::create_rclcpp_instances() {
 
     using std::placeholders::_1;
     const std::string &topic_name = "/mid360";
-    // std::string topic_name = "/livox/lidar";
+    // const std::string &topic_name = "/livox/lidar";
     lidar_pc2_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
         topic_name, 10, std::bind(&ObjectDetection::lidar_callback, this, _1));
 }
-static std::size_t cnt = 0;
+
 void ObjectDetection::lidar_callback(const rclcppCloudSharedPtr msg) {
     RCLCPP_INFO(this->get_logger(), "Pointcloud callback.");
     if (msg->width * msg->height == 0) {
@@ -22,14 +22,14 @@ void ObjectDetection::lidar_callback(const rclcppCloudSharedPtr msg) {
     }
 
     auto cloud{convert_point_cloud2_to_cloud_ptr(msg)};
-
-
-    cnt++;
-
     auto transformed_cloud = rotate(cloud, 0.15);
     test_pc2_pub_->publish(convert_cloud_ptr_to_point_cloud2(transformed_cloud, this));
 
+    OutlierRemoval remover;
+    auto removed_outliers = remover.statistical_pcl(transformed_cloud, 50, 0.01);
+
+
     GroundRemoval algorythm;
-    auto cloud_test = algorythm.planar_segmentation(transformed_cloud, 0.15, 0.01);
+    auto cloud_test = algorythm.planar_segmentation(removed_outliers, 0.15, 0.01);
     rotated_without_ground_pub_->publish(convert_cloud_ptr_to_point_cloud2(cloud_test, this));
 }
